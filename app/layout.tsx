ba291e8233e946d0d2b3731fd1fd8f4edc3a206b
pnrugal-3107 <pn.rugal@gmail.com>
2026-03-26 17:13:53 +0700

    add yandex metric

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,8 +65,31 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Yandex.Metrika counter */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108250247', 'ym');
+
+              ym(108250247, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        {/* /Yandex.Metrika counter */}
       </head>
       <body className={`${inter.className} bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100`}>
+        <noscript>
+          <div style={{ position: 'absolute', left: '-9999px' }}>
+            <img src="https://mc.yandex.ru/watch/108250247" alt="Yandex.Metrika" />
+          </div>
+        </noscript>
         <AuthProvider>
           <AuthInitializer>
             <ScrollToTop />
